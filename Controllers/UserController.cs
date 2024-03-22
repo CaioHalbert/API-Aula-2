50ee3b2fb53b39ddc4c630e8b9ea5a6bb473f747
Logon PF Fiap <logonpflocal@fiap.com.br>
2024-03-22 20:42:06 -0300

    CRUD

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
-﻿using API_Aula_2.Models;
+﻿using API_Aula_2.Data;
+using API_Aula_2.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_Aula_2.Controllers
 {
@@ -8,5 +10,37 @@ namespace API_Aula_2.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        [HttpGet]
+        public IEnumerable<User> getAll()
+        {
+            return _context.Users.ToList();
+        }
+        [HttpPost]
+        public void Add([FromBody] User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+        [HttpPut]
+        public void Update(User entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        [HttpDelete]
+        public void Delete(User entity) 
+        {
+            _context.Users.Remove(entity);
+            _context.SaveChanges();
+        }
+        [HttpGet("/{id}")]
+        public User GetById(int id)
+        {
+            var user = _context.Users.Find(id);
+            
+            return Ok();
+        }
     }
 }
